6115c7e67ed5f4ed9a696ea0b569c094ac22f44f
Thomas Beck <thomas.beck@gmx.at>
2016-03-04 19:00:08 +0100

    initial commit FiboWeb (template-solution)

---
+++ FiboWeb/FiboWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FiboWeb.Global" Language="C#" %>
---
+++ FiboWeb/FiboWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FiboWeb.Startup))]
+namespace FiboWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
